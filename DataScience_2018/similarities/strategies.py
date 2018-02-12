f87da758837076d69d255b0800f24edb1cade98b
Wessel Meijer <wesselmjr@gmail.com>
2018-02-12 19:24:11 +0100

    Fixed NoneType issue
    Fixed issue where two same None's would get passed to the modified queue

--- DataScience_2018/similarities/strategies.py
+++ DataScience_2018/similarities/strategies.py
@@ -1,19 +1,18 @@
 class RecommendationStrategy:
 
   def calculate(self,strategy,p,q):
-    #try:
-      #emptyThing = []
+    try:
       n = 0
       modifiedP = []
       modifiedQ = []
       for item in p:
         if len(q) >= (n + 1):
           n += 1
-          if (type(item) == type(q[n - 1])):
+          if (type(item) == type(q[n - 1])) and (item != None) and (q[n-1] !=  None):
             if (item >= 0 and q[n-1] >= 0):
               modifiedP += [item]
               modifiedQ += [q[n-1]]
 
       return strategy.strategy(modifiedP,modifiedQ)
-    #except:
+    except:
       return "Interface not implemented properly"
