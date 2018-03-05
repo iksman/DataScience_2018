0d9a1213812bdb398f01ada3c373c28e3d746e4a
Wessel Meijer <wesselmjr@gmail.com>
2018-03-05 23:12:19 +0100

    ## added C# functionality * Strategy Pattern * Dictionary Parser

---
+++ CSharp/ConcreteStrategy1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+  class ConcreteStrategy1 : IStrategy
+  {
+    public double algorithm() {
+      return 2.0;
+    }
+  }
+}
---
+++ CSharp/IStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+  interface IStrategy {
+    double algorithm();
+  }
+}
