dbc352089448a43fc9526f3362ab6ddbe65c0433
Wessel Meijer <wesselmjr@gmail.com>
2018-03-08 01:27:19 +0100

    Split off frontend

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,33 +4,10 @@ namespace CSharp {
   class Program {
     static void Main(string[] args) {
       
-      printData(1, 2);
-      printData(1, 3);
-
-
+      Printer.printData(1, 2);
+      Printer.printData(1, 3);
       Console.ReadLine();
-    }
 
-    static void printData(int user1, int user2) { 
-      int[] users = {user1, user2};
-
-      StrategyContext strategy1 = new StrategyContext(new Euclidian());
-      StrategyContext strategy2 = new StrategyContext(new Manhattan());
-      StrategyContext strategy3 = new StrategyContext(new StrategyContext( new StrategyContext(new Pearsson())));
-      var data = DataParser.write2DArray("./userData.data", ",");
-
-      data = DataParser.filterData(data, users[0], users[1], true);
-      var noDelData = DataParser.filterData(data, users[0], users[1], false);
-
-      var tuple = DataParser.splitDictionaries(data);
-      
-      Console.WriteLine("User " + users[0].ToString() + " -VS- User " + users[1].ToString());
-
-      Console.WriteLine("Euclidian - " + strategy1.algorithm(tuple).ToString());
-      Console.WriteLine("Manhattan - " + strategy2.algorithm(tuple).ToString());
-
-      Console.WriteLine("Pearsson - " + strategy3.algorithm(DataParser.splitDictionaries(noDelData)).ToString());
-      Console.WriteLine();
-    }
+    }    
   }
 }
